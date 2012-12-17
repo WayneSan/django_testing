12630bd387b8e325003ce4620985ac34ef0c7fd1
WayneSan <waynesan@zerozone.tw>
2012-12-17 17:00:59 +0100

    Add templates and Poll's views.

--- django_testing/urls.py
+++ django_testing/urls.py
@@ -8,10 +8,19 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'django_testing.views.home', name='home'),
     # url(r'^django_testing/', include('django_testing.foo.urls')),
-
+    
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^polls/', include('django_testing.polls.urls')),
 )
+
+#urlpatterns += patterns('django_testing.polls.views',    
+#    url(r'^polls/$', 'index'),
+#    url(r'^polls/(?P<poll_id>\d+)/$', 'detail'),
+#    url(r'^polls/(?P<poll_id>\d+)/results/$', 'results'),
+#    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'vote'),
+#)
